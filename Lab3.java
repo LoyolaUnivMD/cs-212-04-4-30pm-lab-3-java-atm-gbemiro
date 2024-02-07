72dd1c7ef6a8d75c12d6692f38ee7aed95a7ddc7
aescorcia05 <156947111+aescorcia05@users.noreply.github.com>
2024-02-07 17:19:13 -0500

    Pasted Algorithm as Comments

--- Lab3.java
+++ Lab3.java
@@ -12,8 +12,49 @@ import java.util.Scanner;
 
 class Lab3 {
     public static void main(String[] args) {
-       System.out.println("Welcome to Lab 3");
 
+        //Output "Gooday! Welcome to RO Bank
+        //Create a class template for user with name, balance and current transaction (and methods)
+        //  default balance is 212.90
+        //
+        //main:
+        //Prompt the user to input their name
+        //Store user's name
+        //while transaction is not e
+        //transaction = getTransaction()
+        //switch(transaction)
+        //  case w
+        //    withdraw(getNum(withdrawPrompt))
+        //  case d
+        //    deposit(getNum(depositPrompt)
+        //  case c
+        //    checkBalance()
+        //
+        //
+        //Methods:
+        //
+        //1. getTransaction()
+        //Create array with options: {D, W, C, E}
+        //Ouput options: Deposit, Withdraw, Check balance, Exit
+        //Get user input and store in transaction
+        //While user input is not valid, ask again
+        //
+        //2. deposit(amount)
+        //Add amount to balance
+        //Output deposit confirmation message
+        //Return balance
+        //
+        //3. withdraw(amount)
+        //Subtract amount from balance
+        //Output confirmation message
+        //Return balance
+        //
+        //4. checkBalance()
+        //Output balance
+        //
+        //5. getNum(prompt)
+        //Output prompt
+        //Get user input
 
     }
 }
